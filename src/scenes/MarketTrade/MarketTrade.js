b9ed07efe639f2f6937d002e8eb83c95d04f34c3
Massimo Mannoni <massimo.mannoni@fintech.srl>
2020-10-30 17:11:21 +0100

    Basic Structure

---
+++ src/scenes/MarketTrade/MarketTrade.js
@@ -0,0 +1,11 @@
+import { PureComponent } from "react";
+
+export default class MarketTrade extends PureComponent{
+
+    render(){
+        return (
+            <div>Ciao Maxx</div>
+        )
+    }
+}
+
---
+++ src/scenes/MarketTrade/MarketTradeContainer.js
@@ -0,0 +1,9 @@
+const { Component } = require("react");
+const { default: MarketTrade } = require("./MarketTrade");
+
+class MarketTradeContainer extends Component{
+
+    render() {
+        return <MarketTrade></MarketTrade>
+    }
+}
