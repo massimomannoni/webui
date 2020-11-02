68733bc39cf65ba05832354c7ebed28890d057a1
Massimo Mannoni <massimo.mannoni@fintech.srl>
2020-11-02 09:39:44 +0100

    Added jsonForms

--- src/scenes/MarketTrade/components/ProductTradeForm/ProductTradeForm.js
+++ src/scenes/MarketTrade/components/ProductTradeForm/ProductTradeForm.js
@@ -3,10 +3,26 @@ import React, { Component } from "react";
 class ProductTradeFrom extends Component {
 
   render() {
+
+    // data: The data to be rendered
+    // schema : the JSON schema that describes the underlying data
+    // uischema: the UI schema that describes how the form should be rendered. If none is provided a default layout is used
+    //renderers: The renderers that should be used for rendering the form
+    
+    const data = "";
+    const schema = "";
+    const uiSchema = "";
+
     return (
       <div>
         <div>This Is My From</div>
 
+        <JsonForms
+          schema={schema}
+          uischema={uiSchema}
+          data={data}
+          cells={materialCells}
+        />
       </div>
     );
   }
